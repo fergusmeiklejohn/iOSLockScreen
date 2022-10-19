ab2b08717b8388b2a38cf2b5bf2fa515f8b1fc59
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2022-10-19 15:51:52 +0800

    date, time background & icons

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { useState, useEffect } from "react";
+import wallpaper from "./assets/images/wallpaper.webp";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import dayjs from "dayjs";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ImageBackground source={wallpaper} style={styles.container}>
+      <StatusBar style="light" />
+      <DateAndTime />
+      <Footer />
+    </ImageBackground>
+  );
+}
+
+function DateAndTime() {
+  let [dateObj, setDateObj] = useState(dayjs());
+
+  let date = dateObj.format("dddd, MMMM D");
+  let time = dateObj.format("HH:mm");
+
+  // Update the time every second
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setDateObj(dayjs());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <View style={styles.header}>
+      <Ionicons name="ios-lock-closed" size={24} color="white" />
+      <Text style={styles.date}>{date}</Text>
+      <Text style={styles.time}>{time}</Text>
+    </View>
+  );
+}
+
+function Footer() {
+  return (
+    <View style={styles.footer}>
+      <View style={styles.icon}>
+        <MaterialCommunityIcons name="flashlight" size={24} color="white" />
+      </View>
+      <View style={styles.icon}>
+        <Ionicons name="ios-camera" size={24} color="white" />
+      </View>
     </View>
   );
 }
@@ -13,8 +54,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 250,
+  },
+  date: {
+    color: "#C3FFFE",
+    fontSize: 20,
+    fontWeight: "500",
+    marginTop: 20,
+  },
+  time: {
+    color: "#C3FFFE",
+    fontSize: 82,
+    fontWeight: "bold",
+  },
+
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "auto",
+    height: 85,
+    alignSelf: "stretch",
+    paddingHorizontal: 50,
+    paddingVertical: 10,
+  },
+  icon: {
+    backgroundColor: "#00000050",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
   },
 });
